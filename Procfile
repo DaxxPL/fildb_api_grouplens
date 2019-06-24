13cd48a94a47da58110bbdcb16760427f7ad5c1f
Mikołaj Henklewski <mikolaj.henklewski@wext.it>
2019-06-24 10:27:24 +0200

    herohu prep

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn filmdb_api_grouplens.wsgi --log-file -
--- fimdb_api_grouplens/settings.py
+++ fimdb_api_grouplens/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'njzhsp9!4nx%-!g!@luka27^t#1htlwaq^)j69vwqt@#yng^_o'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
 
 ALLOWED_HOSTS = []
 
