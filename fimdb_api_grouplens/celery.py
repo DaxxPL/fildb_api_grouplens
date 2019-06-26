f77b336349b08c894f12a2cb05f676f02fd6bc93
Mikołaj Henklewski <mikolaj.henklewski@wext.it>
2019-06-26 17:28:36 +0200

    setup update

--- fimdb_api_grouplens/celery.py
+++ fimdb_api_grouplens/celery.py
@@ -2,12 +2,12 @@
 from __future__ import absolute_import
 import os
 from celery import Celery
-
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fimdb_api_grouplens.settings')
 app = Celery('fimdb_api_grouplens')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
